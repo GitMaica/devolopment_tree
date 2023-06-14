062bfa669c26bd8cf805e6b1167b94d58bf89790
Clarke <ajahiclarke19@gmail.com>
2023-06-13 23:29:33 -0500

    menu_changes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const hamburger = document.querySelector(".hamburger")
+const menu = document.querySelector(".menu-wrapper")
+const hambBar = document.querySelector(".menu")
 
 hamburger.addEventListener("click", function(){
     hamburger.classList.toggle("active")
+    menu.classList.toggle("opened")
+    hambBar.classList.toggle("relocate")
 })
