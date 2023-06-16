6d001dfdf16973f6ce695a940d1bad21963605ff
Clarke <ajahiclarke19@gmail.com>
2023-06-15 20:22:28 -0500

    cart_update

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const cartNumber = document.querySelector("#cart-count")
+let count = 0
+
+const cartIncremment = () =>{
+    count += 1
+    cartNumber.innerText = count
+}
+
+const cartDecrement = () =>{
+    if(cartNumber.innerText > 0)
+        count -= 1
+        cartNumber.innerText = count
+}
+
 const hamburger = document.querySelector(".hamburger")
 const menu = document.querySelector(".menu-wrapper")
 const hambBar = document.querySelector(".menu")
