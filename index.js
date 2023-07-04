bd17da36b7b64d8b528f8409d925324a157847ce
Clarke <ajahiclarke19@gmail.com>
2023-07-03 20:49:44 -0500

    dial_dilema

--- index.js
+++ index.js
@@ -1,32 +1,69 @@
 const opendial = document.querySelector(".open-loca-dial")
 const openExpert = document.querySelector(".open-expert-dial")
-const locaDial = document.querySelector(".loca-modal")
+const locaDial = document.querySelector(".loca-dial")
 const expertDial = document.querySelector(".expert-dial")
-const closedial = document.querySelector(".close-loca-dial")
-const insideLocaDial = document.querySelector(".dialog-inside-wrapper")
+const useCurrent = document.querySelector(".current-loca")
+const closeDial = document.querySelector(".click-to-close-loca-dial")
+const closeExpDial = document.querySelector(".click-to-close-expert-dial")
 
 opendial.addEventListener("click", () => {
     locaDial.show()
+    // locaDial.setAttribute('aria-hidden', 'false')
 })
 
-openExpert.addEventListener("click", () => {
-    expertDial.show()
-})
 
-window.addEventListener("click", e => {
+
+window.addEventListener("click", (e) => {
     const dialDimentions = locaDial.getBoundingClientRect()
+    // const dialDimentions2 = expertDial.getBoundingClientRect()
+
+    // const dialHidden = locaDial.getAttribute('aria-hidden')
+    // const expertHidden = expertDial.getAttribute('aria-hidden')
+
+
+    // if(dialHidden === 'false'){
     if(
         e.clientX < dialDimentions.left ||
         e.clientX > dialDimentions.right ||
         e.clientY < dialDimentions.top ||
         e.clientY > dialDimentions.bottom
-    ) {
-        locaDial.close()
-    }
+        ) {
+            locaDial.close()
+            // locaDial.setAttribute('aria-hidden', 'true')
+
+        }
+    // }
+    // if(expertHidden ==='false'){
+    //     if(
+    //         e.clientX < dialDimentions2.left ||
+    //         e.clientX > dialDimentions2.right ||
+    //         e.clientY < dialDimentions2.top ||
+    //         e.clientY > dialDimentions2.bottom
+    //         ) {
+    //             expertDial.close()
+    //             expertDial.setAttribute('aria-hidden', 'true')
+    //         }
+    // }
 })
 
-closedial.addEventListener("click", () => {
+closeDial.addEventListener("click", () =>{
     locaDial.close()
+    // locaDial.setAttribute('aria-hidden', 'true')
+})
+
+useCurrent.addEventListener("click", () => {
+    locaDial.close()
+    // locaDial.setAttribute('aria-hidden', 'true')
+
+})
+
+
+// ?>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+openExpert.addEventListener("click", () => {
+    expertDial.show()
+    // expertDial.setAttribute('aria-hidden', 'false')
+
 })
 
 
@@ -41,9 +78,13 @@ window.addEventListener("click", e => {
             expertDial.close()
         }
     })
-    
-    
-//* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+closeExpDial.addEventListener("click", () =>{
+    expertDial.close()
+    // expertDial.setAttribute('aria-hidden', 'true')
+})
+
+//! >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 const hamburger = document.querySelector(".hamburger")
 const menu = document.querySelector(".menu-wrapper")
@@ -64,10 +105,11 @@ hamburger.addEventListener("click", function(){
 })
 
 
-//* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//! >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-const cartPlus = document.querySelectorAll(".cart-btn")
+// TODO:remove const cartPlus = document.querySelectorAll(".cart-btn")
 const cartNumber = document.querySelector("#cart-count")
+const productBox = document.querySelector(".product-box")
 let count = 0
 
 
@@ -82,8 +124,15 @@ const cartDecrement = () =>{
         cartNumber.textContent = count
 }
 
-for(let i=0; i<cartPlus.length; i++){
-    cartPlus[i].addEventListener("click", function(){
+// for(let i = 0; i < cartPlus.length; i++){
+//     cartPlus[i].addEventListener("click", function(){
+//     })
+// }
+
+productBox.addEventListener("click", (e) =>{
+    const clickedBtn = e.target.classList.contains("cart-btn")
+    if(clickedBtn){
         cartIncremment()
-    })
-}
+    }
+})
+
